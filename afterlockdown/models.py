f127347bb04cdb778f4b22a14a4af0ceda1a70e8
Samkelo Kgosiemang <joymama078@gmail.com>
2020-05-30 18:01:31 +0200

    Add post views

--- afterlockdown/models.py
+++ afterlockdown/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     city = models.CharField(max_length=300, null=False, blank=False)
     body = models.CharField(max_length=160, null=False, blank=False)
+    call_id = models.CharField(max_length=160, null=False, blank=False)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- afterlockdown/views.py
+++ afterlockdown/views.py
@@ -1,3 +1,113 @@
-from django.shortcuts import render
+import random
+import string
 
-# Create your views here.
+from django.contrib import messages
+
+from django.http import Http404, HttpResponse, JsonResponse
+
+from django.shortcuts import redirect, render 
+
+# from .forms import PostSubmitForm
+from .models import Post
+
+
+def get_posts(request):
+    '''
+    Gets and returns all `Post` objects
+    '''
+    submissions = Post.objects.all().order_by('-created')
+    
+    return submissions
+
+def view_post(request, call_id):
+    '''
+    Gets and redirects user to `Post` object associated
+    with `call_id`
+    '''
+    submission = Post.objects.get(call_id=call_id)
+
+    if request.method == 'POST':
+        form = PostSubmitForm(request.POST)
+
+        # Send `form` to create_post view in aferlockdown app.
+        call_id = create_submission(request, form)
+
+        # redirect user to the post detail view after
+        # the creation of new Submission object
+        return redirect('/s/{}'.format(call_id))
+
+    # form to be passed to template if request.method is GET
+    # submission_form = PostSubmitForm() 
+
+    context = {
+        'submission_form': submission_form,
+        'post': submission
+    }
+
+    return render(request, 'posts/post_detailed.html', context)
+
+
+def post_call_id_generator(size=10, chars=string.ascii_letters + string.digits):
+    '''
+    Generates and returns `Submission` base64 call id
+    '''
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def check_call_id_exists(call_id):
+    '''
+    Checks if call id exists. Generates and returns new call id if exists
+    '''
+    # Try/Catch checks to see if a Submission with call_id == call_id
+    # already exists. This will not throw an error if True. And if
+    # that's the case, the function becomes recursive until a unique
+    # call_id is generated
+    try:
+        Post.objects.get(call_id=call_id)
+
+        # New call_id generated
+        new_call_id = post_call_id_generator()
+
+        # New call_id checked against all Submission objects
+        check_call_id_exists(new_call_id)
+    
+    except:
+        # This means there has been an error which means
+        # that there's no Submission object with call_id == call_id
+        return call_id
+
+
+def create_submission(request, form):
+    '''
+    Creates and returns new `Post` object
+    '''
+    if request.method == 'POST':
+        # form passed from POST accepting views
+        form = form
+
+        # Form validation:
+        # 1. Sanitise user input
+        # 2. Check if all fields are filled
+        if not form.is_valid():
+            # Form contained errors
+            messages.error(request, 'Couldn\'t send your submission. Please try again.')
+            return redirect('index')
+        
+        else:
+            # Don't save yet
+            submission = form.save(commit=False)
+
+            # generate call_id
+            _call_id = post_call_id_generator()
+
+            # Check to see if generated call_id exists in DB
+            call_id = check_call_id_exists(_call_id)
+
+            # Assign call_id to Post object
+            submission.call_id = call_id
+
+            # Insert the Submission object into DB
+            form.save()
+
+            messages.success(request, 'Success')
+            return call_id
