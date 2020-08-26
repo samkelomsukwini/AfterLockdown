cc638318232e0075a2ed606d40746d97f58a9037
Samkelo Kgosiemang <joymama078@gmail.com>
2020-08-26 14:45:23 +0000

    Replace f strings with format function in twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -14,14 +14,26 @@ import tweepy
 from afterlockdown.models import Post
 
 
-auth = tweepy.OAuthHandler('foLrwYNAr3Vhru873F9KDIgzC', 'wT5txGO8vx3ODimsnYAZLdRrt15YvDkmjqe50uPY9L1iRaoTdx')
-auth.set_access_token('1265214128051892226-s17SqFTBnmQ8OOjqQQdepD5YmvISXp', 'bvK7dkQb2a1Th4oaOoYh8yS4GUrYwfscIFt0RldMT3EoZ')
 
-api = tweepy.API(auth)
+def twitter_bot():
+    auth = tweepy.OAuthHandler('foLrwYNAr3Vhru873F9KDIgzC', 'wT5txGO8vx3ODimsnYAZLdRrt15YvDkmjqe50uPY9L1iRaoTdx')
+    auth.set_access_token('1265214128051892226-s17SqFTBnmQ8OOjqQQdepD5YmvISXp', 'bvK7dkQb2a1Th4oaOoYh8yS4GUrYwfscIFt0RldMT3EoZ')
 
-post = Post.objects.order_by('?').first()
+    api = tweepy.API(auth)
+
+    post = Post.objects.order_by('?').first()
 
 
-public_tweets = api.update_status(status=f'{post.body} http://samkelo.me/s/{post.call_id} #lockdown #covid19', place_id='Ekhaya LamaSukwini')
+    public_tweets = api.update_status(status='{} http://samkelo.me/s/{} #lockdown #covid19'.format(post.body, post.call_id), place_id='Ekhaya LamaSukwini')
 
-print('Done')
+    print('Done')
+
+
+
+def main():
+    twitter_bot()
+
+
+
+if __name__ == '__main__':
+    main()
